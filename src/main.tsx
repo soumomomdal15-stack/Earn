8bff11f5e6e7e9e1807f83e45e19856a0084b519
soumomomdal15-stack <soumomomdal15@gmail.com>
2026-07-05 20:37:31 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB6VmvDtFKYLgW9C_9WE-YoE3LNh19_utk",
   authDomain: "earnos-780db.firebaseapp.com",
   projectId: "earnos-780db",
   storageBucket: "earnos-780db.firebasestorage.app",
   messagingSenderId: "469051623753",
-  appId: "1:469051623753:web:397a832378de1663603298",
-  measurementId: "G-FC5ZR50LEQ"
+  appId: "1:469051623753:web:397a832378de1663603298"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
