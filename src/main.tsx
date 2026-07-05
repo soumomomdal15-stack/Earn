bfa1ccb9debb7b07f586cb861df4b67a51884580
soumomomdal15-stack <soumomomdal15@gmail.com>
2026-07-05 20:11:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6VmvDtFKYLgH9C_9WE-YoE3LhHi9_utk",
+  authDomain: "earnos-780db.firebaseapp.com",
+  projectId: "earnos-780db",
+  storageBucket: "earnos-780db.firebasestorage.app",
+  messagingSenderId: "469051623753",
+  appId: "1:469051623753:web:397a832378de1663603298",
+  measurementId: "G-FC5ZRS0LEG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
